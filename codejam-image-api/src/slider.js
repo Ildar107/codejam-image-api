c6479dbbe1b776aba9899bbbb8c56965b4f69425
Ildar107 <ildar107wk@gmail.com>
2019-11-18 23:38:05 +0300

    feat: add eslint

--- codejam-image-api/src/slider.js
+++ codejam-image-api/src/slider.js
@@ -1,42 +1,39 @@
 
-
 export default class Slider {
-    constructor() {
-        this.values = [32, 64,128, 256, 512];  
-        this.rangeSlider = document.getElementById("rs-range-line");
-        this.rangeBullet = document.getElementById("rs-bullet");
-        this.rangeBulletValue = document.getElementById("rs-bullet-value");
-        this.maxRealValue = this.rangeSlider.max;
-        
-    }
+  constructor() {
+    this.values = [32, 64, 128, 256, 512];
+    this.rangeSlider = document.getElementById('rs-range-line');
+    this.rangeBullet = document.getElementById('rs-bullet');
+    this.rangeBulletValue = document.getElementById('rs-bullet-value');
+    this.maxRealValue = this.rangeSlider.max;
+  }
 
-    Init(value) {
-        this.rangeSlider.value = value;
-        this.value = this.values[this.rangeSlider.value];
-        this.setBulletPosition();
-        this.rangeSlider.addEventListener("input", () => {
-            this.value = this.values[this.rangeSlider.value];
-            this.setBulletPosition();
-        })
-    }
+  Init(value) {
+    this.rangeSlider.value = value;
+    this.value = this.values[this.rangeSlider.value];
+    this.setBulletPosition();
+    this.rangeSlider.addEventListener('input', () => {
+      this.value = this.values[this.rangeSlider.value];
+      this.setBulletPosition();
+    });
+  }
 
-    setBulletPosition () {
-        this.rangeBulletValue.innerHTML = this.values[this.rangeSlider.value];
-        let bulletPosition = (this.rangeSlider.value /this.rangeSlider.max);
-        this.rangeBullet.style.left = (bulletPosition * 490) + "px";
-    }
+  setBulletPosition() {
+    this.rangeBulletValue.innerHTML = this.values[this.rangeSlider.value];
+    const bulletPosition = (this.rangeSlider.value / this.rangeSlider.max);
+    this.rangeBullet.style.left = `${bulletPosition * 490}px`;
+  }
 
-    getValue () { 
-        return this.value;
-    }
+  getValue() {
+    return this.value;
+  }
 
-    getRealValue() { 
-        this.rangeSlider.value;
-    }
+  getRealValue() {
+    return this.rangeSlider.value;
+  }
 
-    onChange(func) {
-        this.rangeSlider.addEventListener('input', func);
-        return this;
-    }
+  onChange(func) {
+    this.rangeSlider.addEventListener('input', func);
+    return this;
+  }
 }
-
--- codejam-image-api/src/user.js
+++ codejam-image-api/src/user.js
@@ -1,6 +1,6 @@
 export default class User {
-    constructor(login, avatarUrl){
-        this.login = login;
-        this.avatarUrl = avatarUrl;
-    }
+  constructor(login, avatarUrl) {
+    this.login = login;
+    this.avatarUrl = avatarUrl;
+  }
 }
